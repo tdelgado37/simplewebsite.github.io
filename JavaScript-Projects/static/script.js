5c96e252d25c46ed4c6a1327afeb6af4e75e26b8
tommy.delgado <tdelgado37.td@gmail.com>
2019-07-08 22:02:07 -0400

    removing console logs

--- JavaScript-Projects/static/script.js
+++ JavaScript-Projects/static/script.js
@@ -101,7 +101,7 @@ var copyAllButtons = [];
 for (let i=0; i< all_buttons.length; i++){
   copyAllButtons.push(all_buttons[i].classList[1]);
 }
-console.log(copyAllButtons);
+
 function buttonColorChange(buttonColor){
   if(buttonColor.value == 'red'){
     buttonsRed();
